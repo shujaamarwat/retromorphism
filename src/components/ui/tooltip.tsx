39273d11531605e793c7e618a5d7a4a034aa42e6
Shujaa Marwat <56261790+shujaamarwat@users.noreply.github.com>
2025-06-28 18:01:06 +0500

    Add RetroMorphic arrow to tooltip component

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "../../lib/utils"
+import { cn } from "@/lib/utils"
 
 const TooltipProvider = TooltipPrimitive.Provider
 
@@ -17,11 +17,18 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "z-50 overflow-hidden rounded-xl border-2 border-[#001428] bg-white-100 px-3 py-2 text-sm font-text-16-med text-black-100 shadow-[0_4px_0_#001428,0_6px_8px_rgba(0,20,40,0.2)] animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      "z-50 overflow-hidden rounded-xl border-2 border-[#001428] bg-white-100 px-4 py-3 text-sm font-text-16-med text-black-100 shadow-[0_4px_0_#001428,0_6px_8px_rgba(0,20,40,0.2)] animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
     )}
     {...props}
-  />
+  >
+    {props.children}
+    <TooltipPrimitive.Arrow 
+      className="fill-white-100 stroke-[#001428] stroke-2" 
+      width={16} 
+      height={8}
+    />
+  </TooltipPrimitive.Content>
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
